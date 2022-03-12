f1f2140ee90ccf1194f43d92fdda06879586cb44
Yuto Moriyasu <y.moriyasu@icloud.com>
2022-03-12 21:44:10 +0900

    create example question styles

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,40 @@
 import data from "../src/data"
+import { Box, Button, Container, Typography } from "@mui/material"
+import Image from "next/image"
 
 const Home = () => {
-  const example = data[0].id
+  const id = data[0].id
+  const choices = data[0].choices
   return (
     <>
-      <h1>Quizy with React</h1>
-      <p>{example}</p>
+      <Container maxWidth="sm">
+        <Box sx={{mt: 5}}>
+          <Typography variant="h6" align="center" fontWeight="bold">
+            ガチで東京の人しか解けない！ #東京の難読地名クイズ
+          </Typography>
+        </Box>
+        <Box sx={{mt: 6}}>
+          <Typography fontWeight="bold">
+            { id }. この地名はなんて読む？
+          </Typography>
+          <Box sx={{
+            mt: 3,
+            display: "flex",
+            justifyContent: "center"
+          }}>
+            <Image src="/img/kuizy01.png" width="620" height="372" />
+          </Box>
+          <Box sx={{width: '100%'}}>
+            {choices.map(choice => {
+              return (
+                <Box sx={{width: '100%', mb: 2}}>
+                  <Button variant="outlined" fullWidth="true" sx={{justifyContent: 'start', py: 1, fontWeight: 'bold'}}>{choice}</Button>
+                </Box>
+              )
+            })}
+          </Box>
+        </Box>
+      </Container>
     </>
   )
 }
